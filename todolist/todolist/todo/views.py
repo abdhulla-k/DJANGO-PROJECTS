59f9059b095acc7c7a0ebf247fb727465b8e6c46
abdhulla-k <abdhullak149@gmail.com>
2021-11-04 18:01:13 +0530

    index function completed, completed index.html,  now the function will get all data from user and save it to data base.

--- todolist/todolist/todo/views.py
+++ todolist/todolist/todo/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    todo = Todo.objects.all
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html', {'todos': todo})
