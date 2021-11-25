001df76fc357299bc3f31da62cd198147404fa6d
abdhulla-k <abdhullak149@gmail.com>
2021-11-25 23:34:17 +0530

    Email sent

--- SENDEMAILS/sendemails/app/views.py
+++ SENDEMAILS/sendemails/app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def home(request):
+
+    send_mail('Hey',
+    'This is a mail from Django for test meil sending',
+    'abdhullak149@gmil.com',
+    ['abdhullak96@gmil.com'],
+    fail_silently=False)
+
     return render(request, 'home.html')
