ad9b51b6b7069fe1583ae24dce3ffa12e11e7dee
abdhulla-k <abdhullak149@gmail.com>
2022-03-11 00:48:17 +0530

    created home url

--- LearnDjango/storefront/storefront/playground/urls.py
+++ LearnDjango/storefront/storefront/playground/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Home, name = 'Home'),
     path('hello', views.SayHello)
 ]
--- LearnDjango/storefront/storefront/playground/views.py
+++ LearnDjango/storefront/storefront/playground/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def Home(request):
+    return render(request, 'Home.html')
+
 def SayHello(request):
    # return HttpResponse('Hello world')
    return render(request , 'hello.html', {'name':'Abdhu'})
