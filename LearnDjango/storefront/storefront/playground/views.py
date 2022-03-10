8080da211051ee885fcd54c9255848dc387aa21f
abdhulla-k <abdhullak149@gmail.com>
2022-03-10 23:40:08 +0530

    created templates and html file

--- LearnDjango/storefront/storefront/playground/views.py
+++ LearnDjango/storefront/storefront/playground/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def SayHello(request):
-    return HttpResponse('Hello world')
+   # return HttpResponse('Hello world')
+   return render(request , 'hello.html')
