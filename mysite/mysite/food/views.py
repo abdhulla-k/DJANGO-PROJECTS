2a57fb86f1a0b225953c47779aa3d5a877f178b7
abdhulla-k <abdhullak149@gmail.com>
2022-03-22 18:00:16 +0530

    created template 'index.html'

--- mysite/mysite/food/views.py
+++ mysite/mysite/food/views.py
@@ -1,8 +1,10 @@
 import imp
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello world')
+    return render(request, 'food/index.html')
