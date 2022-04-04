4464a00f9e9c82a47edd9d962bc95604271bb76e
abdhulla-k <abdhullak149@gmail.com>
2022-04-04 16:12:17 +0530

    created email field to registration form

--- mysite/mysite/register/views.py
+++ mysite/mysite/register/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm( request.POST )
+        form = RegisterForm( request.POST )
         if form.is_valid():
             form.save()
             user_name = form.cleaned_data.get( 'username' )
             messages.success( request, f'Welcome {user_name}. Your account is successfully created')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render( request, 'user/register.html', {'form' : form} )
